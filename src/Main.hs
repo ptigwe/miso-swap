2e41e7c72bc0fbb42c213c0eb97b1244b6ed1419
Tobenna P. Igwe <ptigwe@gmail.com>
2017-09-13 13:56:12 +0100

    Add Multiplicative Model

--- src/Main.hs
+++ src/Main.hs
@@ -9,21 +9,22 @@ module Main where
 
 import qualified Additive as A
 import Data.Function
-import qualified Data.Map as M
 import Data.Monoid
+import qualified Multiplicative as M
 import qualified SubModel
 
 import Miso
 import Miso.String
 
 data Model = Model
-  { additive :: A.Model
+  { additive :: M.Model
   , currentModel :: String
   } deriving (Show, Eq)
 
 data Action
   = Swap
   | AdditiveAction !A.Action
+  | MultiplicativeAction !M.Action
   | NoOp
 
 main :: IO ()
@@ -36,13 +37,13 @@ main = do
     subs = []
 
 initialModel :: Model
-initialModel = Model A.initialModel "Additive"
+initialModel = Model M.initialModel "Additive"
 
-additivePa :: A.PublicActions Action
-additivePa = A.PublicActions {A.toParent = AdditiveAction, A.click = NoOp}
+additivePa :: M.PublicActions Action
+additivePa = M.PublicActions {M.toParent = MultiplicativeAction, M.click = NoOp}
 
 updateModel :: Action -> Model -> Effect Action Model
-updateModel (AdditiveAction act) m = do
+updateModel (MultiplicativeAction act) m = do
   addModel <- SubModel.updateModel additivePa act $ additive m
   noEff m {additive = addModel}
 updateModel Swap m = noEff m
@@ -54,5 +55,5 @@ display m@Model {..} =
     []
     [ button_ [onClick Swap] [text "Swap"]
     , p_ [] [text . pack $ "Model : ", b_ [] [text . pack $ currentModel]]
-    , SubModel.viewModel additivePa A.NoOp additive
+    , SubModel.viewModel additivePa M.NoOp additive
     ]
---
+++ src/Multiplicative.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Multiplicative where
+
+import Miso
+import Miso.String
+import SubModel
+
+data Model = Model
+  { counter :: Int
+  , clickCount :: Int
+  } deriving (Eq, Show)
+
+initialModel :: Model
+initialModel = Model 1 0
+
+data PublicActions action = PublicActions
+  { toParent :: Action -> action
+  , click :: action
+  }
+
+data Action
+  = Multiply
+  | Divide
+  | NoOp
+  deriving (Show, Eq)
+
+instance SubModel PublicActions Action Model where
+  updateModel pa action m@Model {..} =
+    case action of
+      Multiply ->
+        m {counter = counter * 2} <# do
+          putStrLn $ "Multiply" ++ show counter
+          pure $ click pa
+      Divide ->
+        m {counter = max 1 $ counter `div` 2} <# do
+          putStrLn $ "Divide" ++ show counter
+          pure $ click pa
+      NoOp -> noEff m
+  viewModel pa act m =
+    div_
+      []
+      [ button_ [onClick $ toParent pa Multiply] [text "*"]
+      , text . ms . show . counter $ m
+      , button_ [onClick $ toParent pa Divide] [text "/"]
+      ]
